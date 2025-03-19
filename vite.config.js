d1879be921a5d350136e07891653aa3ec35bad66
serhaterdem <serhaterdem961@gmail.com>
2025-03-19 23:59:21 +0300

    live-server implementation

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import open from 'open';
+import path from 'path'; // Add this import
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'open-external-url',
+      configureServer() {
+        // Open the external URL after a brief delay to allow the server to start
+        setTimeout(() => {
+          open('https://harmonyhaventest.erdemserhat.com/');  // Change this to your desired URL
+        }, 1000); // You can adjust the timeout duration if necessary
+      },
+    },
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -12,21 +24,22 @@ export default defineConfig({
   },
   base: '',
   server: {
-    port: 5173
+    allowedHosts: ['harmonyhaventest.erdemserhat.com'],
+    port: 3000,
   },
   preview: {
-    port: 4173
+    port: 4173,
   },
   build: {
     rollupOptions: {
       output: {
-        manualChunks: undefined
-      }
+        manualChunks: undefined,
+      },
     },
     // SPA için gerekli yapılandırma
     assetsDir: 'assets',
     outDir: 'dist',
     emptyOutDir: true,
-    sourcemap: true
-  }
+    sourcemap: true,
+  },
 })
