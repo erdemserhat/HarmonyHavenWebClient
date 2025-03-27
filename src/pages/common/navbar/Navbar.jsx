2e9ee908b6cbd60d3654c8225e946337b82eea07
serhaterdem <serhaterdem961@gmail.com>
2025-03-27 06:43:56 +0300

    chat bot feature

--- src/pages/common/navbar/Navbar.jsx
+++ src/pages/common/navbar/Navbar.jsx
@@ -5,6 +5,7 @@ import './Navbar.css'
 import icoImage from '../../../assets/ico.png'
 import axios from "@/services/api/axios.js"
 import {useAuthChecker} from "@/context/AuthContext.jsx";
+import { FaComments } from 'react-icons/fa';
 
 export function Navbar() {
     const navigate = useNavigate()
@@ -89,11 +90,17 @@ export function Navbar() {
                         Makaleler
                     </Link>
                     {isAuthenticated && (
-                        <NavLink to="/notifications" className="nav-link notification-button" onClick={() => setIsMenuOpen(false)}>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
-                            </svg>
-                        </NavLink>
+                        <>
+                            <Link to="/chat" className="nav-link" onClick={() => setIsMenuOpen(false)}>
+                                <FaComments className="nav-icon" />
+                                Chat
+                            </Link>
+                            <NavLink to="/notifications" className="nav-link notification-button" onClick={() => setIsMenuOpen(false)}>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
+                                </svg>
+                            </NavLink>
+                        </>
                     )}
                     {isAuthenticated ? (
                         <div className="profile-dropdown-container" ref={dropdownRef}>
