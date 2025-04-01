1713348d955c421762b80828c6f43238ae01c82f
serhaterdem <serhaterdem961@gmail.com>
2025-04-01 19:15:03 +0300

    increased timeout ms

--- src/services/api/axios.js
+++ src/services/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const instance = axios.create({
     withCredentials: true,
     baseURL: 'https://harmonyhavenappserver.erdemserhat.com/api/v1',
-    timeout: 10000,
+    timeout: 20000,
     headers: {
         'Content-Type': 'application/json',
         'harmonyhavenapikey': 'harmonyhavenapikeyK12yW8CBSoBfy41Cu6b3UDbMfEjijD0cKp4K166z29CADrYT4nRtOolZlOxGaOL5X7wmXY9fqyiFRvLeCB2OYS6J9x5zYbtSuiqsieelAD2lo9'
