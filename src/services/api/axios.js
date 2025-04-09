92f8e38175c99201403695a4e9ad248e9f1541f8
serhaterdem <serhaterdem961@gmail.com>
2025-04-09 19:36:20 +0300

    mobil responsive features

--- src/services/api/axios.js
+++ src/services/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 //
 const instance = axios.create({
     withCredentials: true,
-    baseURL: 'https://harmonyhavenappserver.erdemserhat.com/api/v1',
+    baseURL: 'http://localhost:8080/api/v1',
     timeout: 20000,
     headers: {
         'Content-Type': 'application/json',
